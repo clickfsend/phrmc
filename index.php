aa77867ab6307f95577f8a837b9e2c722f31e3d6
rxtopsale <70957962+rxtopsale@users.noreply.github.com>
2021-09-15 11:33:13 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
-<?php
-require_once dirname(__FILE__) . '/kclient.php';
-$client = new KClient('https://prtnr.rxtopsale.com/api.php?', 'YZRPQBJBQqq3tngW');
-$client->restoreFromQuery();
-?>
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>404</title>
+</head>
+<body>
+  <p>404</p>
+  <span id="ef7a9d9a-25f9-4592-495d-7486b5e2b001"></span><script type="application/javascript">
+var d=document;var s=d.createElement('script'); 
+s.src='https://prtnr.rxtopsale.com/DqsNdncy?se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+'&frm6141ceff97a0d=script6141ceff97a0e&_cid=ef7a9d9a-25f9-4592-495d-7486b5e2b001'; 
+if (document.currentScript) { 
+document.currentScript.parentNode.insertBefore(s, document.currentScript);
+} else {
+d.getElementsByTagName('head')[0].appendChild(s);
+}
+if (document.location.protocol === 'https:' && 'https://prtnr.rxtopsale.com/DqsNdncy?se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+'&frm6141ceff97a0d=script6141ceff97a0e'.indexOf('http:') === 0 ) {alert('The website works on HTTPS. The tracker must use HTTPS too.');}
+</script>
+</body>
+</html>
